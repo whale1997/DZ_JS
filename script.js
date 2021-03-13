5ee6fe8b3d1a7e4d39d01c11ea1f22309c54c437
whale1997 <80566174+whale1997@users.noreply.github.com>
2021-03-13 13:27:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var tc = prompt('Введите температуру в градусах по Цельсию', '');
+var tf = 1.8 * tc + 32;
+alert ('Температура в градусах по Фаренгейту равна ' + tf);
+
+var admin, name;
+var name = prompt('Введите значение переменной name ', '');
+var admin = name;
+alert ('Значение переменной admin ' + admin);
